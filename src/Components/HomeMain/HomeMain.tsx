61f44884d2ab4303cdc5a0565d36501c636f67cf
SriSabarish2021 <srisabarish003@gmail.com>
2025-05-13 12:12:00 +0530

    sd

--- src/Components/HomeMain/HomeMain.tsx
+++ src/Components/HomeMain/HomeMain.tsx
@@ -1,7 +1,67 @@
+import { useEffect } from 'react';
 import '/src/Styles/HomeMain.css'
 import { IoIosStar } from "react-icons/io";
 
 const HomeMain = () => {
+
+
+  useEffect(() => {
+    
+
+    let getelementone=document.querySelector('.home-main-for-intro')
+    let getanimateelementone=document.querySelector('.smallcontent-one-home')
+    let observer=new IntersectionObserver(([entry])=>{
+      if (entry.intersectionRatio>0.7) {
+        console.log('heloo');
+        if (getanimateelementone) {
+                  getanimateelementone?.classList.add('smallcontent-one-home-move')
+
+        }
+        
+      }
+    
+    },{threshold:0.7})
+
+    if (getelementone) {
+          observer.observe(getelementone)
+    }
+
+
+
+    let getelementtwo=document.querySelector('.home-main-for-intro')
+    let getanimateelementtwo=document.querySelector('.smallcontent-two-home')
+    let observertwo=new IntersectionObserver(([entry])=>{
+      if (entry.intersectionRatio>0.7) {
+        console.log('heloo');
+        if (getanimateelementtwo) {
+                  getanimateelementtwo?.classList.add('smallcontent-two-home-move')
+
+        }
+        
+      }
+      
+    },{threshold:0.7})
+
+    if (getelementtwo) {
+          observertwo.observe(getelementtwo)
+    }
+     return () => {
+      if (getelementone) {
+          observer.unobserve(getelementone)
+      } 
+      if (getelementtwo) {
+            observertwo.unobserve(getelementtwo)
+      }     
+  }
+    
+    
+  })
+  
+
+
+  
+ 
+
   return (
     <div className='home-main-container'>
         <div className='home-main-for-intro'>
@@ -10,6 +70,31 @@ const HomeMain = () => {
               
             </div>
             <div className='man-image-in-home'></div>
+
+            <div className='smalldesign-two-home'></div>
+            <div className='smalldesign-four-home'></div>
+            <div className='smalldesign-five-home'></div>
+
+            <div className='smallcontent-one-home'>
+              <div className='smallcontent-img'>
+                <img className='img-for-small-cont' src="./HomeImg/read.png" alt="" />
+              </div>
+              <div className='smallcontent-content'>
+                <p className='p-one-small-cont'>10K</p>
+                <p  className='p-two-small-cont'>Students</p>
+              </div>
+              
+            </div>
+            <div className='smallcontent-two-home'>
+              <div className='smallcontent-img'>
+                <img className='img-for-small-cont' src="./HomeImg/online-learning.png" alt="" />
+              </div>
+              <div className='smallcontent-content'>
+                <p className='p-one-small-cont'>20+</p>
+                <p  className='p-two-small-cont'>Online Courses</p>
+              </div>
+              
+            </div>
           </div>
           <div className='home-content-div'>
             <div className='tit-for-home-content'>
@@ -33,12 +118,16 @@ const HomeMain = () => {
                 <p className='home-qual-para'><span className='star-span'><IoIosStar/><IoIosStar/> <IoIosStar/> <IoIosStar/> <IoIosStar/> </span>5-Star ratings on G2, Discord and More</p>
               </div>
             </div>
+
+            <div className='smalldesign-one-home'></div>
+            <div className='smalldesign-three-home'></div>
           </div>
           <p className='learn-p-in-bottom'>LEARN FUNDAMENTALS</p>
           <div className='light-design-one'></div>
           <div className='light-design-two'></div>
           <div className='light-design-three'></div>
           <div className='light-design-four'></div>
+           <div className='light-design-five'></div>
         </div>
         <div className='home-main-for-design'>
           <div className='div-for-bottom-image-clody'></div>
