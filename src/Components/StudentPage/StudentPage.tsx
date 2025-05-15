4ae453cca4f48231ed69bba62efe6c814a12d8ff
SriSabarish2021 <srisabarish003@gmail.com>
2025-05-15 15:45:31 +0530

    adsd

--- src/Components/StudentPage/StudentPage.tsx
+++ src/Components/StudentPage/StudentPage.tsx
@@ -1,12 +1,64 @@
+import { useEffect } from 'react';
 import '/src/Styles/StudentPage.css'
+import { FaArrowRight } from "react-icons/fa6";
 
 const StudentPage = () => {
+
+      useEffect(() => {
+        
+    
+        const getelementoneinstud=document.querySelector('.student-page-container') as HTMLElement;
+        const getanimateelementoneinstud=document.querySelector('.scale-img-one-stud') as HTMLElement;       const getanimateelementtwoinstud=document.querySelector('.scale-img-two-stud') as HTMLElement;
+        const getanimateelementthreeinstud=document.querySelector('.scale-img-three-stud') as HTMLElement;
+
+        const observerinstud=new IntersectionObserver(([entry])=>{
+          if (entry.intersectionRatio>0.4) {
+            if (getanimateelementoneinstud) {
+                getanimateelementoneinstud.classList.add('noscale-img-stud')
+            }
+            if (getanimateelementtwoinstud) {
+                getanimateelementtwoinstud.classList.add('noscale-img-stud')
+            }
+            if (getanimateelementthreeinstud) {
+                getanimateelementthreeinstud.classList.add('noscale-img-stud')
+            }
+            
+          }
+          else{
+            if (getanimateelementoneinstud) {
+                getanimateelementoneinstud.classList.remove('noscale-img-stud')
+            }
+            if (getanimateelementtwoinstud) {
+                getanimateelementtwoinstud.classList.remove('noscale-img-stud')
+            }
+            if (getanimateelementthreeinstud) {
+                getanimateelementthreeinstud.classList.remove('noscale-img-stud')
+            }
+          }
+        
+        },{threshold:0.4})
+    
+        if (getelementoneinstud) {
+              observerinstud.observe(getelementoneinstud)
+        }
+    
+    
+         return () => {
+          
+          if (getelementoneinstud) {
+                observerinstud.unobserve(getelementoneinstud)
+          }     
+      }
+        
+        
+      },[])
+      
   return (
     <div className="student-page-container">
         <div className='student-page-inner-container'>
             <div className="student-page-image-container">
-                <img  className='student-page-image-one' src='./StydentPage/stupage-img-one.webp' alt="Student Image"/>
-                <img  className='student-page-image-two' src='./StydentPage/stu-img-two.webp' alt="Student Image"/>
+                <img  className='student-page-image-one scale-img-one-stud ' src='./StydentPage/stupage-img-one.webp' alt="Student Image"/>
+                <img  className='student-page-image-two scale-img-two-stud' src='./StydentPage/stu-img-two.webp' alt="Student Image"/>
             </div>
             <div className="student-page-content-container">
                 <div className='student-page-title-bar'>
@@ -18,7 +70,7 @@ const StudentPage = () => {
                 </div>
                 <div className='student-page-main-content'>
                     <div className='image-bar-in-main-content'>
-                        <div className='content-image-bar-in-main-cont'></div>
+                        <div className='content-image-bar-in-main-cont scale-img-three-stud'></div>
                         <div className='stud-enroll-image-bar-in-main-cont'>
                             <div className='stu-enroll-image'>
                                 <div className='stu-img stu-img-one'></div>
@@ -49,7 +101,7 @@ const StudentPage = () => {
                             <div className='stu-thinking-analysis'>
                                 <div className='stu-thinking-one'>
                                     <div className='stu-thinking-image'>
-                                        <div className='stu-thinking-img'>
+                                        <div className='stu-thinking-img img-one-stud-think'>
                                             <img className='img-for-main-analysis-two' src="./StydentPage/light-bulb.png" alt="" />
                                         </div>
                                     </div>
@@ -60,7 +112,7 @@ const StudentPage = () => {
                                 </div>
                                 <div className='stu-thinking-two'>
                                     <div className='stu-thinking-image'>
-                                        <div className='stu-thinking-img'>
+                                        <div className='stu-thinking-img img-two-stud-think'>
                                             <img className='img-for-main-analysis-two' src="./StydentPage/diploma.png" alt="" />
                                         </div>
                                     </div>
@@ -72,14 +124,23 @@ const StudentPage = () => {
                             </div>
                         </div>
                         <div className='stud-content-main-btn'>
-                            <button className='stud-analysis-btn'>View Programs</button>
+                            <button className='stud-analysis-btn'>View Programs <span className='span-for-btn-stud-page'><FaArrowRight/> </span><div className='design-div-for-tud-btn'></div></button>
 
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div className='design-element-stud-page'></div>
         </div>
 
+        <div className='stud-design-main'></div>
+
+        <div className='stud-round-design-one'>
+            <div  className='stud-round-design-two'>
+
+            </div>
+        </div>
     </div>
   )
 }
