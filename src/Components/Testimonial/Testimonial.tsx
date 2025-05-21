3131fe02b11717932f8514eb19dbd834f03e6837
SriSabarish2021 <srisabarish003@gmail.com>
2025-05-21 16:05:39 +0530

    sdsds

--- src/Components/Testimonial/Testimonial.tsx
+++ src/Components/Testimonial/Testimonial.tsx
@@ -1,9 +1,57 @@
+import { useState,useEffect,useRef } from 'react';
 import '/src/Styles/Testimonial.css'
 import { FaStar } from "react-icons/fa";
 import { FaAngleLeft } from "react-icons/fa6";
 import { FaAngleRight } from "react-icons/fa6";
 
 const Testimonial = () => {
+
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+   
+    
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft -= 780; 
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+             const valueofscroll:number=scrollRef.current.scrollWidth - scrollRef.current.clientWidth
+
+      if (scrollRef.current.scrollLeft>=valueofscroll) {
+         scrollRef.current.scrollLeft = 0;
+      }else{
+      scrollRef.current.scrollLeft += 780;
+
+      }
+    }
+  };
+
+  useEffect(() => {
+    
+    const scrollContainer = scrollRef.current;
+
+  const timer = setInterval(() => {
+    if (scrollContainer) {
+      const maxScrollLeft = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+
+      if (scrollContainer.scrollLeft >= maxScrollLeft) {
+        
+        scrollContainer.scrollLeft = 0;
+      } else {
+        
+        scrollContainer.scrollLeft += 780;
+      }
+    }
+  }, 5000);
+    return () => {
+      clearInterval(timer)
+    }
+  })
+  
+
   return (
     <div className='testimonial-container'>
       <div className='tesitimonial-top-design'>
@@ -29,27 +77,60 @@ const Testimonial = () => {
             </div>
           </div>
           <div className='testimonail-main-content-bar'>
-            <div className='testimonial-inner-content-bar'>
-              <div className='student-testimonial-one'>
-              <div className='student-star-rating'>
-                <FaStar/><FaStar/><FaStar/><FaStar/><FaStar/>
-              </div>
-              <div className='student-comment'>
-                <p className='comment-student'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit dolorum magnam mollitia aut aliquam dolorem praesentium. Quisquam ut ex et voluptates reiciendis cum quibusdam similique ipsam, pariatur tenetur sed. Ullam?</p>
-              </div>
-              <div className='student-info'>
-                <div className='student-image-tag'></div>
-                <div className='student-info-tag'>
-                  <p className='student-name-para'>Velu Mani</p>
-                  <p className='student-degree-para'>Computer Science</p>
+            <div className='testimonial-inner-content-bar' ref={scrollRef}>
+             
+                <div className='student-testimonial-one'>
+                <div className='student-star-rating'>
+                  <FaStar/><FaStar/><FaStar/><FaStar/><FaStar/>
                 </div>
-              </div>
-              </div>
+                <div className='student-comment'>
+                  <p className='comment-student'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit dolorum magnam mollitia aut aliquam dolorem praesentium. Quisquam ut ex et voluptates reiciendis cum quibusdam similique ipsam, pariatur tenetur sed. Ullam?</p>
+                </div>
+                <div className='student-info'>
+                  <div className='student-image-tag'></div>
+                  <div className='student-info-tag'>
+                    <p className='student-name-para'>Velu Mani</p>
+                    <p className='student-degree-para'>Computer Science</p>
+                  </div>
+                </div>
+                </div>
+                <div className='student-testimonial-one'>
+                <div className='student-star-rating'>
+                  <FaStar/><FaStar/><FaStar/><FaStar/><FaStar/>
+                </div>
+                <div className='student-comment'>
+                  <p className='comment-student'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit dolorum magnam mollitia aut aliquam dolorem praesentium. Quisquam ut ex et voluptates reiciendis cum quibusdam similique ipsam, pariatur tenetur sed. Ullam?</p>
+                </div>
+                <div className='student-info'>
+                  <div className='student-image-tag'></div>
+                  <div className='student-info-tag'>
+                    <p className='student-name-para'>Velu Mani</p>
+                    <p className='student-degree-para'>Computer Science</p>
+                  </div>
+                </div>
+                </div>
+                <div className='student-testimonial-one'>
+                <div className='student-star-rating'>
+                  <FaStar/><FaStar/><FaStar/><FaStar/><FaStar/>
+                </div>
+                <div className='student-comment'>
+                  <p className='comment-student'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit dolorum magnam mollitia aut aliquam dolorem praesentium. Quisquam ut ex et voluptates reiciendis cum quibusdam similique ipsam, pariatur tenetur sed. Ullam?</p>
+                </div>
+                <div className='student-info'>
+                  <div className='student-image-tag'></div>
+                  <div className='student-info-tag'>
+                    <p className='student-name-para'>Velu Mani</p>
+                    <p className='student-degree-para'>Computer Science</p>
+                  </div>
+                </div>
+                </div>
+             
+              
             </div>
 
             <div className='testimonial-move-button'>
-              <button className='testimonial-left-btn'><FaAngleLeft className='btn-cont'/></button>
-              <button className='testimonial-right-btn'><FaAngleRight  className='btn-cont'/></button>
+              <button className='testimonial-left-btn' onClick={()=>scrollLeft()}><FaAngleLeft className='btn-cont'/></button>
+              <button className='testimonial-right-btn' onClick={()=>scrollRight()}><FaAngleRight  className='btn-cont'/></button>
             </div>
             
           </div>
