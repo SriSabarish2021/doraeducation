f2f728f959366e0bd185f32b24f526f12896822d
SriSabarish2021 <srisabarish003@gmail.com>
2025-05-27 16:28:28 +0530

    ssss

---
+++ src/Components/AboutPageComponents/Tutor.tsx
@@ -0,0 +1,65 @@
+import '../../Styles/AboutPageCSS/Tutro.css'
+
+const Tutor = () => {
+  return (
+    <div className='tutor-about-page-container'>
+      <div className='tutor-elements'>
+        <div className='tutor-box tutor-title-box'>
+            <div className='tutor-indicator '>
+                <p>Tutor</p>
+                <div className='tutor-indicator-design-bar-one'></div>
+                <div className='tutor-indicator-design-bar-two'></div>
+            </div>
+            <p className='tutor-page-para'>Meet the Experts <span>Who Inspire and</span><span>Educate.</span></p>
+            <button className='btn-for-tutor-page'>View More</button>
+        </div>
+        <div className='tutor-box  tutor-box-one'>
+            <div className='tutor-img'>
+            </div>
+            <div className='tutor-img-design'></div>
+            <div className='name-tutor-box'></div>
+            <div className='info-tutor-box'></div>
+
+        </div>
+        <div className='tutor-box tutor-box-two'>
+            <div className='tutor-img tutor-img-two' >
+                
+            </div>
+              <div className='tutor-img-design'></div>
+              <div className='name-tutor-box'></div>
+            <div className='info-tutor-box'></div>
+
+        </div>
+        <div className='tutor-box tutor-box-three'>
+            <div className='tutor-img tutor-img-three'>
+                
+            </div>
+              <div className='tutor-img-design'></div>
+              <div className='name-tutor-box'></div>
+            <div className='info-tutor-box'></div>
+
+        </div>
+        <div className='tutor-box tutor-box-four'>
+             <div className='tutor-img tutor-img-four'>
+                
+            </div>
+              <div className='tutor-img-design'></div>
+              <div className='name-tutor-box'></div>
+            <div className='info-tutor-box'></div>
+
+        </div>
+        <div className='tutor-box tutor-box-five'>
+           <div className='tutor-img tutor-img-five'>
+                
+            </div>
+              <div className='tutor-img-design'></div>
+              <div className='name-tutor-box'></div>
+            <div className='info-tutor-box'></div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Tutor
--- src/Components/WebsiteComponents/AboutPage.tsx
+++ src/Components/WebsiteComponents/AboutPage.tsx
@@ -1,11 +1,13 @@
 import AboutPageCourse from "../AboutPageComponents/AboutPageCourse"
 import AboutTop from "../AboutPageComponents/AboutTop"
+import Tutor from "../AboutPageComponents/Tutor"
 
 const AboutPage = () => {
   return (
     <div className="about-page-main-container">
       <AboutTop/>
       <AboutPageCourse/>
+      <Tutor/>
     </div>
   )
 }
