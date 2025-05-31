614ddf07e308fd85952b84c842576a848906686e
SriSabarish2021 <srisabarish003@gmail.com>
2025-05-31 13:33:09 +0530

    ssss

---
+++ src/Components/CoursePageComponents/TopShortCourse.tsx
@@ -0,0 +1,144 @@
+import { useEffect } from 'react'
+import '../../Styles/CoursePageCSS/TopShortCourse.css'
+import { PiClock } from "react-icons/pi";
+import { PiReadCvLogo } from "react-icons/pi";
+import { FaRegHeart } from "react-icons/fa"; 
+const TopShortCourse = () => {
+
+ 
+
+
+
+      const cards = document.querySelectorAll(".course-box") ;
+
+      const stackArea = document.querySelector(".top-short-course-page-container");
+
+      function rotateCards() {
+        let angle = 0;
+        cards.forEach((card, index) => {
+          if (card.classList.contains("away")) {
+            card.style.transform = `translateY(-120vh) rotate(-48deg)`;
+          } else {
+            card.style.transform = ` rotate(${angle}deg)`;
+            angle = angle - 10;
+            card.style.zIndex = cards.length - index;
+          }
+        });
+      }
+
+      useEffect(() => {
+         rotateCards();
+      
+        
+      })
+      
+     
+
+      window.addEventListener("scroll", () => {
+        const distance = window.innerHeight/2;
+
+        if(stackArea){
+          const topVal = stackArea.getBoundingClientRect().top;
+           let index = -1 * (topVal / distance + 1);
+            index = Math.floor(index);
+             for (let i = 0; i < cards.length; i++) {
+            if (i <= index) {
+              cards[i].classList.add("away");
+            } else {
+              cards[i].classList.remove("away");
+            }
+          }
+        }
+ 
+        rotateCards();
+      });
+  return (
+    <div className='top-short-course-page-container'>
+      <div className='top-short-course-title-container'>
+        <div className='course-page-top-course-title'>
+            <p>Top Coruses that</p>
+            <p>Provided in</p>
+            <p>Mindspire</p>
+        </div>
+        <p className='para-in-course-page'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, ea tempora voluptatum nemo quod assumenda. Possimus nam distinctio sunt voluptates.</p>
+        <button className='btn-in-course-page-title'>View Course</button>
+       
+      </div>
+      <div className='top-short-course-content-container'>
+        <div className='course-box course-box-one'>
+          <div className='course-box-head-div'>
+            <p>JavaScript</p>
+            <p>Mastery</p>
+          </div>
+          <div className='course-box-course-info-div'>
+            <p className='para-in-box-time'><PiClock/>6 months</p>
+            <p className='para-in-box-lesson'><PiReadCvLogo/>10 Lesson</p>
+          </div>
+          <div  className='course-box-btn-div'>
+            <button className='apply-btn-course-box'>Apply</button>
+            <button className='like-btn-course-box'><FaRegHeart/></button>
+          </div>
+        </div>
+        <div className='course-box course-box-two'>
+           <div className='course-box-head-div'>
+            <p>TypeScript</p>
+            <p>Core</p>
+          </div>
+          <div className='course-box-course-info-div'>
+            <p className='para-in-box-time'><PiClock/>6 months</p>
+            <p className='para-in-box-lesson'><PiReadCvLogo/>10 Lesson</p>
+          </div>
+          <div  className='course-box-btn-div'>
+            <button className='apply-btn-course-box'>Apply</button>
+            <button className='like-btn-course-box'><FaRegHeart/></button>
+          </div>
+        </div>
+        <div className='course-box course-box-three'>
+           <div className='course-box-head-div'>
+            <p>CSS</p>
+            <p>Mastery</p>
+          </div>
+          <div className='course-box-course-info-div'>
+            <p className='para-in-box-time'><PiClock/>6 months</p>
+            <p className='para-in-box-lesson'><PiReadCvLogo/>10 Lesson</p>
+          </div>
+          <div  className='course-box-btn-div'>
+            <button className='apply-btn-course-box'>Apply</button>
+            <button className='like-btn-course-box'><FaRegHeart/></button>
+          </div>
+        </div>
+        <div className='course-box course-box-four'>
+           <div className='course-box-head-div'>
+            <p>Backend</p>
+            <p>Learning Path</p>
+          </div>
+          <div className='course-box-course-info-div'>
+            <p className='para-in-box-time'><PiClock/>6 months</p>
+            <p className='para-in-box-lesson'><PiReadCvLogo/>10 Lesson</p>
+          </div>
+          <div  className='course-box-btn-div'>
+            <button className='apply-btn-course-box'>Apply</button>
+            <button className='like-btn-course-box'><FaRegHeart/></button>
+          </div>
+        </div>
+        <div className='course-box course-box-five'>
+           <div className='course-box-head-div'>
+            <p>Node.js</p>
+            <p>Mastery</p>
+          </div>
+          <div className='course-box-course-info-div'>
+            <p className='para-in-box-time'><PiClock/>6 months</p>
+            <p className='para-in-box-lesson'><PiReadCvLogo/>10 Lesson</p>
+          </div>
+          <div  className='course-box-btn-div'>
+            <button className='apply-btn-course-box'>Apply</button>
+            <button className='like-btn-course-box'><FaRegHeart/></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TopShortCourse
+
--- src/Components/WebsiteComponents/CoursePage.tsx
+++ src/Components/WebsiteComponents/CoursePage.tsx
@@ -1,9 +1,10 @@
+import '../../Styles/CoursePageCSS/CoursePageMain.css'
+import TopShortCourse from "../CoursePageComponents/TopShortCourse"
 
 const CoursePage = () => {
   return (
-    <div>
-      <h1>helooo</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae saepe sequi delectus reiciendis? Quia eius, excepturi omnis consequuntur esse iure molestias quidem nostrum ex delectus quos dolor quo dicta quibusdam. Odio, nostrum quidem repudiandae porro harum excepturi! Eaque repudiandae, libero nam tempora corporis, pariatur voluptas, eveniet quaerat placeat impedit soluta ab ea vel. Inventore sunt dolore quod dignissimos. Accusantium praesentium et quae rem eveniet ex, commodi natus dicta perspiciatis tenetur, pariatur asperiores aut. Omnis ratione odit exercitationem facilis laborum, corrupti temporibus ducimus quas. Ab maxime consequatur error autem illum consectetur voluptas doloribus omnis molestiae laborum? Assumenda reiciendis officiis saepe nemo.</p>
+    <div className="course-page-main-container">
+      <TopShortCourse/>
     </div>
   )
 }
