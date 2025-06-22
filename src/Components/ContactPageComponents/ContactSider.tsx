f1377b394d4a56a208cb807638fa738f58ebd931
SriSabarish2021 <srisabarish003@gmail.com>
2025-06-22 12:41:23 +0530

    sdsdsdd

--- src/Components/ContactPageComponents/ContactSider.tsx
+++ src/Components/ContactPageComponents/ContactSider.tsx
@@ -1,10 +1,28 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import '../../Styles/ContactPageCSS/ContactSider.css'
 
 const ContactSider = () => {
  
+  const [contactwidth,setcontactwidth]=useState(0)
+    useEffect(() => {
+  
+  
+      setcontactwidth(window.innerWidth)
+      
+     
+      
+       return () => {
+        setcontactwidth(0)
+      }
+    
+    
+    }, [])
   
   useEffect(() => {
+
+
+
+    
 
     const mainelement=document.querySelector('.contact-side-bar-conatainer')
 
@@ -14,40 +32,86 @@ const ContactSider = () => {
 
     const boxone=document.querySelector('.contact-sider-box-one')
     const boxtwo=document.querySelector('.contact-sider-box-three')
-    const observer=new IntersectionObserver(([entry])=>{
-      if (entry.intersectionRatio>0.5) {
-          itemone?.classList.add('no-logo-contact-move')
-          itemtwo?.classList.add('no-logo-contact-move')
-          itemthree?.classList.add('no-logo-contact-move')
-          boxone?.classList.add('no-box-move-contact')
-          boxtwo?.classList.add('no-box-move-contact')
-      }
-      else{
-          itemone?.classList.remove('no-logo-contact-move')
-          itemtwo?.classList.remove('no-logo-contact-move')
-          itemthree?.classList.remove('no-logo-contact-move')
-          boxone?.classList.remove('no-box-move-contact')
-          boxtwo?.classList.remove('no-box-move-contact')
-      }
-    },{threshold:0.5})
+
+    const boxonemob=document.querySelector('.contact-sider-left-one')
+    const boxtwomob=document.querySelector('.contact-sider-left-two')
+    const boxthreemob=document.querySelector('.contact-sider-left-three')
+    const boxfourmob=document.querySelector('.contact-sider-left-four')
     
-    if (mainelement) {
-      observer.observe(mainelement)
+
+    let observer;
+
+    if (Number(contactwidth)>930) {
+          observer=new IntersectionObserver(([entry])=>{
+          if (entry.intersectionRatio>0.5) {
+              itemone?.classList.add('no-logo-contact-move')
+              itemtwo?.classList.add('no-logo-contact-move')
+              itemthree?.classList.add('no-logo-contact-move')
+              boxone?.classList.add('no-box-move-contact')
+              boxtwo?.classList.add('no-box-move-contact')
+          }
+          else{
+              itemone?.classList.remove('no-logo-contact-move')
+              itemtwo?.classList.remove('no-logo-contact-move')
+              itemthree?.classList.remove('no-logo-contact-move')
+              boxone?.classList.remove('no-box-move-contact')
+              boxtwo?.classList.remove('no-box-move-contact')
+          }
+        },{threshold:0.5})
+        
+        if (mainelement) {
+          observer.observe(mainelement)
+        }
     }
+    else{
+          observer=new IntersectionObserver(([entry])=>{
+          if (entry.intersectionRatio>0.5) {
+              itemone?.classList.add('no-logo-contact-move')
+              itemtwo?.classList.add('no-logo-contact-move')
+              itemthree?.classList.add('no-logo-contact-move')
+              boxonemob?.classList.add('no-box-move-contact')
+              boxtwomob?.classList.add('no-box-move-contact')
+              boxthreemob?.classList.add('no-box-move-contact')
+              boxfourmob?.classList.add('no-box-move-contact')
+
+          }
+          else{
+              itemone?.classList.remove('no-logo-contact-move')
+              itemtwo?.classList.remove('no-logo-contact-move')
+              itemthree?.classList.remove('no-logo-contact-move')
+              boxonemob?.classList.remove('no-box-move-contact')
+              boxtwomob?.classList.remove('no-box-move-contact')
+              boxthreemob?.classList.remove('no-box-move-contact')
+              boxfourmob?.classList.remove('no-box-move-contact')
+          }
+        },{threshold:0.5})
+        
+        if (mainelement) {
+          observer.observe(mainelement)
+        }
+    }
+    
   
     return () => {
-      if (mainelement) {
+      if (Number(contactwidth)>930) {
+        if (mainelement) {
       observer.unobserve(mainelement)
     }
+      }else{
+          if (mainelement) {
+        observer.unobserve(mainelement)
+        }
+      }
+      
     }
-  }, [])
+  }, [contactwidth])
   
 
   
 
   return (
     <div className="contact-side-bar-conatainer">
-      <div className='contact-side-box contact-sider-box-one'>
+      <div className={`contact-side-box  ${Number(contactwidth)>930?'contact-sider-box-one':'contact-sider-left-one'}`}>
         <div className='contact-sider-logo'>
           <div className='contact-logo-one'>
             <img src="./ContactPageImage/location.png" alt="location-logo" className='contact-logo' />
@@ -64,7 +128,7 @@ const ContactSider = () => {
           </div>
         </div>
       </div>
-      <div className='contact-side-box contact-sider-box-two'>
+      <div className={ `contact-side-box  ${Number(contactwidth)>930?'contact-sider-box-two':'contact-sider-right-one'}`}>
         <div className='contact-sider-logo contact-inner-logo-two'>
           <div className='contact-logo-two'>
             <img src="./ContactPageImage/call.png" alt="location-logo" className='contact-logo' />
@@ -81,9 +145,26 @@ const ContactSider = () => {
           </div>
         </div>
       </div>
-      <div className='contact-side-box contact-sider-box-three'>
+      <div className={`contact-side-box ${Number(contactwidth)>930?'contact-sider-box-three':'contact-sider-left-two'}`}>
         <div className='contact-sider-logo'>
           <div className='contact-logo-three'>
+            <img src="./ContactPageImage/message (1).png" alt="location-logo" className='contact-logo' />
+          </div>
+        </div>
+        <div className='contact-sider-cont'>
+          <div className='contact-title'>
+            <p>Email</p>
+          </div>
+          <div className='contact-content'>
+            <p>srisabarish003@gmail.com</p>
+            <p>sabarishsri315@gmail.com</p>
+           
+          </div>
+        </div>
+      </div>
+      <div className={`contact-side-box  contact-sider-box-four${Number(contactwidth)>930?'':'contact-sider-right-two'}`}>
+        <div className='contact-sider-logo contact-inner-logo-two'>
+          <div className='contact-logo-two'>
             <img src="./ContactPageImage/message (1).png" alt="location-logo" className='contact-logo' />
           </div>
         </div>
--- src/Components/WebsiteComponents/ContactPage.tsx
+++ src/Components/WebsiteComponents/ContactPage.tsx
@@ -14,10 +14,10 @@ const ContactPage = () => {
     }, [])
   return (
     <div  className="contact-page-main-container">
-        <ContactTop/>
-        <ContactSider/>
+        {/* <ContactTop/>
+        <ContactSider/> */}
         <ContactConsult/>
-        <ContactTouch/>
+       {/*  <ContactTouch/> */}
     </div>
   )
 }
